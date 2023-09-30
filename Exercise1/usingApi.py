7b43f1e9fe884d8b424254cafc84b540fd4e4b68
carlosleon <carlosleon@gapp.nthu.edu.tw>
2023-09-29 21:20:49 -0600

    Fixing getEmployee() and creating getEmployee(id)

--- Exercise1/usingApi.py
+++ Exercise1/usingApi.py
@@ -3,14 +3,23 @@
 import requests
 import json
 
-# 1) Getting Employee Data
-def getEmployee():
-    #Well... it seems that adding 'headers' solve the issue of 'Mod Security'
-    headers = {
+#Well... it seems that adding 'headers' solve the issue of 'Mod Security'
+headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
-    }
+}
+
+# 1) Getting All Employee Data
+def getEmployees():
     #routing
-    employee_src = 'https://dummy.restapiexample.com/api/v1/employees'
+    employees_src = 'https://dummy.restapiexample.com/api/v1/employees'
+    #Get response and converting data into json
+    return json.loads(requests.get(employees_src, headers=headers).text)
+
+
+# 2) Getting a Single Employees
+def getEmployee(id):
+    #routing
+    employee_src = 'https://dummy.restapiexample.com/api/v1/employee/' + str(id)
     #Get response and converting data into json
     return json.loads(requests.get(employee_src, headers=headers).text)
 
@@ -18,8 +27,6 @@ def getEmployee():
 
 
 
-
-
-
 #Testing Area
-#print(getEmployee())
+#print(getEmployees())
+print(getEmployee(1))
