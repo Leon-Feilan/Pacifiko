e1268d83ddabbd0d690da692a17e61339dad2904
carlosleon <carlosleon@gapp.nthu.edu.tw>
2023-09-29 23:03:41 -0600

    #creating a list of dictionaries (products)

---
+++ Exercise2/Python/productSearch.py
@@ -0,0 +1,23 @@
+
+#creating a list of dictionaries (products)
+productsList = [
+    {'product_id':1,
+    'product_name': 'Cereal',
+    'price': 20},
+    {'product_id':2,
+    'product_name': 'Pizza',
+    'price': 40},
+    {'product_id':3,
+    'product_name': 'Milk',
+    'price': 5},
+    {'product_id':4,
+    'product_name': 'Apple',
+    'price': 5},
+    {'product_id':5,
+    'product_name': 'Ice cream',
+    'price': 10},
+    {'product_id':6,
+    'product_name': 'Tacos',
+    'price': 10},
+    ]
+
