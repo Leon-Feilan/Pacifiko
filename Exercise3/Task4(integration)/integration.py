8010ef4b7c1c3831c5bae69382108d00a32ee3fe
carlosleon <carlosleon@gapp.nthu.edu.tw>
2023-09-30 22:17:03 -0600

    Adding some UI and updating readme.md

--- Exercise3/Task4(integration)/integration.py
+++ Exercise3/Task4(integration)/integration.py
@@ -28,13 +28,43 @@ def getProductByName(name):
     print(data)
 
 def setOrder(customer_id):
-    #data = pd.read_sql_query("SELECT * FROM dbo.Orders", connection)
+    data = pd.read_sql_query("SELECT * FROM dbo.Orders", connection)
     statement = "INSERT INTO dbo.Orders VALUES ("+ str(len(data)+2) + ","+str(customer_id)+","+"'"+str(date.today())+"')"
     ##pd.read_sql_query wouldn't work with inserting values, so we need to use a cursor (something simmilar to a pointer)
     connection.cursor().execute(statement)
     data = pd.read_sql_query("SELECT * FROM dbo.Orders", connection)
-    #print(data)
+    print(data)
 
+def orderUI():
+    #print a message
+    data = pd.read_sql_query("SELECT * FROM dbo.Customers", connection)
+    print('Hey! These are our costumers, please remember their id for creating an order\n')
+    print(data)
+    id = int(input('\n Please Insert CostumerID: '))
+    setOrder(id)
+    print('\n Done!')
+
+def mainUI():
+    exit=False
+    print("\t Welcome!\n")
+    while(not exit):
+        option = int(input('Please select an action (insert number): \n 1.Display Products and Stock \n 2.Search Products By Name \n 3.Place an Order\n 4.Exit \n : '))
+        if(option > 3):
+            exit=True
+            break
+        else:
+            if(option==1):
+                getProductList()
+            elif(option==2):
+                getProductByName(str(input('What is the name of the product?: ')))
+            else:
+                orderUI()
+
+if __name__ == "__main__":
+    mainUI()
+
+
+#Testing Area
 #getProductList()
 #getProductByName('Jelly')
-setOrder(3)
+#setOrder(3)
